3ed015381d6da35a38e764b6208ad10b36e90abd
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-03-13 18:11:52 -0300

    chore: fix user creation with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :comments, as: :commentable, dependent: :destroy
 end
