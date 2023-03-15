2a8639840770682cfb340a80db9f373dd4896d28
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-03-15 15:33:05 -0300

    chore: setup user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@
 
 class UsersController < ApplicationController
   def index; end
+
+  def show
+    @user = User.find_by(username: params[:username])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, path: 'auth'
+
+  resources :users, only: %i[index show], param: :username
+
   root to: 'users#index'
 end
